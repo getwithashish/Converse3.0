9ac81dc87f0c1685b16d29f18a084c36d553fb42
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-07-16 11:28:24 +0530

    feat: Add file containing static messages

---
+++ converse_backend/messages.py
@@ -0,0 +1,8 @@
+USERNAME_REQUIRED = "Username is required"
+PASSWORD_REQUIRED = "Password is required"
+USERNAME_EXISTS = "Username already exists"
+USER_REGISTRATION_SUCCESSFUL = "User has been successfully registered"
+
+INVALID_USERNAME_PASSWORD = "Username or Password is invalid"
+
+INVALID_PROMPT = "Provided prompt is invalid"
