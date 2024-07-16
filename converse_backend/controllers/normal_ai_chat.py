9bc9518a9a84b394e0411bdd8280cd1c2dda0512
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-07-16 16:06:22 +0530

    feat: Add controllers for normal AI Chat
    - Get chat history list
    - Get a particular chat history
    - Delete a particular chat history
    - Delete all chats

--- converse_backend/controllers/normal_ai_chat.py
+++ converse_backend/controllers/normal_ai_chat.py
@@ -2,8 +2,14 @@ from flask import request
 from flask_jwt_extended import get_jwt, jwt_required
 
 from client.ai_models.gemini.gemini_ai_model import GeminiAIModel
-from messages import INVALID_PROMPT
-from service.normal_chatter import NormalChatter
+from utils.common_response import CommonResponse
+from messages import (
+    CHAT_HISTORY_LIST_RETRIEVED,
+    CHAT_HISTORY_RETRIEVED,
+    CHAT_HISTORY_UNAVAILABLE,
+    INVALID_PROMPT,
+)
+from converse_backend.service.normal_ai_chatter import NormalAiChatter
 from utils.chat_response import ChatResponse
 
 
@@ -15,7 +21,7 @@ def NormalAiChat():
 
     if prompt:
         gemini_ai_model = GeminiAIModel()
-        normal_chatter = NormalChatter(gemini_ai_model)
+        normal_chatter = NormalAiChatter(gemini_ai_model)
         ai_response, chat_id = normal_chatter.chat(prompt, chat_id, user_id)
         status = 200
     else:
@@ -23,3 +29,41 @@ def NormalAiChat():
         status = 400
 
     return ChatResponse(ai_response, chat_id, status).format_response()
+
+
+@jwt_required()
+def NormalAiChatHistoryList():
+    user_id = get_jwt().get("user_id", None)
+
+    normal_chatter = NormalAiChatter()
+    chat_list = normal_chatter.get_chat_history_list(user_id)
+    status = 200
+
+    return CommonResponse(CHAT_HISTORY_LIST_RETRIEVED, status, chat_list)
+
+
+@jwt_required()
+def NormalAiChatHistory():
+    user_id = get_jwt().get("user_id", None)
+    chat_id = request.args.get("chat_id", None)
+
+    normal_chatter = NormalAiChatter()
+    chat_history = normal_chatter.get_chat_history(user_id, chat_id)
+    if chat_history:
+        status = 200
+        return CommonResponse(CHAT_HISTORY_RETRIEVED, status, chat_history)
+
+    status = 404
+    return CommonResponse(CHAT_HISTORY_UNAVAILABLE, status, None)
+
+
+@jwt_required()
+def deleteNormalAiChatHistory():
+    user_id = get_jwt().get("user_id", None)
+    chat_id = request.args.get("chat_id", None)
+
+    normal_chatter = NormalAiChatter()
+    normal_chatter.delete_chat_history(user_id, chat_id)
+
+    status = 204
+    return "", status
--- converse_backend/messages.py
+++ converse_backend/messages.py
@@ -6,3 +6,7 @@ USER_REGISTRATION_SUCCESSFUL = "User has been successfully registered"
 INVALID_USERNAME_PASSWORD = "Username or Password is invalid"
 
 INVALID_PROMPT = "Provided prompt is invalid"
+
+CHAT_HISTORY_LIST_RETRIEVED = "Chat history list has been successfully retrieved"
+CHAT_HISTORY_RETRIEVED = "Chat history has been successfully retrieved"
+CHAT_HISTORY_UNAVAILABLE = "Chat history is not available"
