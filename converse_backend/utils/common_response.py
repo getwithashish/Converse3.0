54f1a19c159a34d0aeea3ffdd343b1f68fc6b17f
getwithashish <ashishsamtgeorge@mca.ajce.in>
2024-07-16 16:13:29 +0530

    feat: Add 'data' field to the Common Response

--- converse_backend/utils/common_response.py
+++ converse_backend/utils/common_response.py
@@ -3,12 +3,13 @@ from flask import jsonify
 
 class CommonResponse:
 
-    def __init__(self, message, status) -> None:
+    def __init__(self, message, status, data=None) -> None:
         self.message = message
         self.status = status
+        self.data = data
 
     def format_response(self):
         return (
-            jsonify({"message": self.message}),
+            jsonify({"message": self.message, "data": self.data}),
             self.status,
         )
